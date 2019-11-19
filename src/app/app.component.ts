d672d411fe3c81fc66a7bc8b4dad985afe1efbd7
Gabriel Touron <gabriel.touron@gmail.com>
2019-11-19 15:52:24 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my awesome app';
+  isAuth = true;
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = false;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout');
+  }
+}
+
